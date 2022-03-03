e0114fd480b17ec249017c557c3ce97eb2426b9c
Lakshita-Garg <lakshita.garg@ibm.com>
2022-03-03 11:13:55 +0530

    Add project files.

---
+++ Day4C/CollAdv.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//-------------------------Stack--------------------------------------
+//namespace Day4C
+//{
+//    internal class CollAdv
+//    {
+//        static void Main(string[] args)
+//        {
+//            var value = new Stack<int>();
+//            value.Push(10);
+//            value.Push(20);
+//            value.Push(30);
+//            value.Push(40);
+//            value.Push(50);
+//            Console.WriteLine("Popped value from the stack: " + value.Pop());
+//            Console.WriteLine("Last inserted value in the stack: "+value.Peek());
+//            Console.WriteLine("The values in the stack are:");
+//            foreach (var item in value) 
+//                Console.WriteLine(item);
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
+
+
+
+//-------------------------Queue--------------------------------------
+//namespace Day4C
+//{
+//    internal class CollAdv
+//    {
+//        static void Main(string[] args)
+//        {
+//            var value = new Queue<int>();
+//            value.Enqueue(10);
+//            value.Enqueue(20);
+//            value.Enqueue(30);
+//            value.Enqueue(40);
+//            value.Enqueue(50);
+//            Console.WriteLine("First inserted value in the queue: " + value.Dequeue());
+//            Console.WriteLine("First value present in the queue: " + value.Peek());
+//            Console.WriteLine("The values in the queue are:");
+//            foreach (var item in value)
+//                Console.WriteLine(item);
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
+
+
+
+//-----------------------------Dictionary---------------------------------
+//namespace Day4C
+//{
+//        internal class CollAdv
+//    {
+//        static void Main(string[] args)
+//        {
+//            var pair = new Dictionary<string,string>();
+//                pair.Add("al", "Aluminium");
+//                pair.Add("fe", "Iron");
+//                pair.Add("si", "Silicon");
+//                pair.Add("h", "Hydrogen");
+//                pair.Add("n", "Nitrogen");
+//                pair.Add("he", "helium");
+//                pair.Add("se", "Selenium");
+//            Console.WriteLine("The keys in the dictionary are:");
+//            foreach (var item in pair.Keys)
+//                Console.WriteLine(item);
+//            Console.WriteLine("The values in the dictionary are:");
+//            foreach (var item in pair.Values)
+//                Console.WriteLine(item);
+//            Console.WriteLine("The key/value pairs in the dictionary are:");
+//            foreach (var item in pair)
+//                Console.WriteLine(item);
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
+
+//---------------------------Hashtable---------------------------------
+namespace Day4C
+{
+    internal class CollAdv
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("al", "Aluminium");
+            ht.Add("fe", "Iron");
+            ht.Add("si", "Silicon");
+            ht.Add("h", "Hydrogen");
+            ht.Add("n", "Nitrogen");
+            ht.Add("he", "Helium");
+            ht.Add("se", "Selenium");
+            ICollection k = ht.Keys;
+            Console.WriteLine("The hashtable conatins the following key/value pairs:");
+            foreach (string key in k)
+                Console.WriteLine(key+": "+ht[key]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day4C/Nullables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//---------------------------------Nullables------------------------------
+namespace Day4C
+{
+    internal class Nullables
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> i = null;
+            int? j = null;
+            double? k = null;
+            bool? v = null;
+            int? x = 12;
+            int? y = i ?? j;
+            int? z = x ?? j;
+            Console.WriteLine("The nullable int values are: i = {0}, j = {1}" ,i,j);
+            Console.WriteLine("The nullable double value is: " + k);
+            Console.WriteLine("The nullable boolean value is: " + v);
+            Console.WriteLine("After using Null coalescing operator: y = {0}, z = {1}", y, z);
+            Console.ReadLine();
+        }
+    }
+}
